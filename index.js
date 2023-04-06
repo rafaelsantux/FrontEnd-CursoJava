4422048028a8d38892bfd80c7a8e0c9e5a989b9a
rafaelsantux <rafaelsantux0411@gmail.com>
2023-04-05 21:33:27 -0300

    Updates no JavaScript.

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ $(document).ready(myApp)
 
 function myApp() {
 
-    console.log("Funciona!")
+    // Carrega a pagina inicial
+    loadpage("user")
+}
 
+//Carrega uma pagina.
+/* O caminho final da pagina é passado como parametro da funçao. */
+function loadpage(page) {
+    
+    //Monta os caminhos para os componentes da pagina solicitada.
+    const path = {
+        html: `/pages/${page}/index.html`,
+        css: `/pages/${page}/index.css`,
+        js: `/pages/${page}/index.js`
+    }
+
+    $.get(path.html)
+        .done((data)=> {
+            $("main").html(data)
+        })
+        .fail((error) => {
+            loadpage('e404')
+        })
 }
