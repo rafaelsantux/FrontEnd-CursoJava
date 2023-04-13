9ef9566bb8af31afe1e12c6a90f08bef691963c2
rafaelsantux <rafaelsantux0411@gmail.com>
2023-04-13 18:31:36 -0300

    Update Api
    Adicionando API Json-server para rodar o back-end e API html-server para rodar o front-end.

---
+++ api/index.js
@@ -0,0 +1,105 @@
+//* ATENÇÃO! Lembre-se que este aplicativo será substituído pela nossa própria
+//* API REST no momento oportuno.
+//**/
+
+// Carrega e inicializa o json-server e suas dependências.
+const jsonServer = require('json-server')
+
+// Cria um novo aplicativo (objeto) json-server.
+const server = jsonServer.create()
+
+// Define o arquivo "JSON" (banco de dados fake).
+const router = jsonServer.router('db.json')
+
+// Carrega o recurso de "middlewares" do json-server.
+const middlewares = jsonServer.defaults()
+
+// Ativa o recurso de "middlewares" do json-server.
+server.use(middlewares)
+
+// Define a porta HTTP do json-server.
+const port = 3000
+
+/**
+* Extrai os dados da requisição para tratamento da API.
+* Isso é necessário para os métodos POST e PUT porque eles enviam dados para 
+* a API na forma de JSON que são convertidos (parse) para Object.
+**/
+server.use(jsonServer.bodyParser)
+
+/**
+* Intercepta e altera os dados vindos do front-end antes de enviá-los para a 
+* API json-server.
+*/
+server.use((req, res, next) => {
+
+ // Se esta enviando um novo contato...
+ if (req.method == 'POST' && req.path == '/contacts') {
+
+   // Obtém a data atual do sistema.
+   const today = new Date()
+
+   // Formata a data para 'system date' (aaaa-mm-dd hh:ii:ss).
+   sysdate = today.toISOString().replace('T', ' ').split('.')[0]
+
+   // Inclui a data no 'body' da requisição.
+   req.body.date = sysdate
+
+   /**
+    * Renderiza a 'view' do json-server.
+    * Isso altera a saída padrão do json-server para customizá-la.
+    **/
+   router.render = (req, res, next) => {
+
+     // Se um novo contato foi enviado...
+     if (req.method == 'POST' && req.path == '/contacts') {
+
+       // Se o novo contato foi gravado com sucesso...
+       if (res.locals.data.id > 0) {
+
+         /** 
+          * Responde com o JSON.
+          * O importante aqui é { "status" : "success" }, o restante é opcional.
+          **/
+         res.json({
+           status: 'success',
+           data: {
+             message: 'Contato salvo',
+             HTTPcode: res.statusCode,
+             id: res.locals.data.id
+           }
+         })
+
+         // Se houve falha ao gravar contato...
+       } else {
+
+         /** 
+          * Responde com o JSON.
+          * O importante aqui é { "status" : "error" }, o restante é opcional.
+          **/
+         res.json({
+           status: 'error',
+           data: {
+             message: 'Falha ao gravar contato'
+           }
+         })
+       }
+     }
+     return
+   }
+
+ }
+
+ // Encerra 'server.use()', gravando as alterações.
+ next()
+})
+
+
+
+// Executa a API do json-server.
+server.use(router)
+
+// Escutando requisições.
+server.listen(port, () => {
+ console.log(`JSON Server: http://localhost:${port}/`)
+})
--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,33 +1,71 @@
+// Executa 'myContacts()' quando o documento estiver pronto.
 $(document).ready(myContacts)
 
 /**
  * Função principal da página "contacts".
  **/
 function myContacts() {
+
     /**
      * Altera o título da página quando 'contacts' for acessada.
+     * Usa a função sendContact() definida em '/index.js'.
      **/
-    changeTitle('Faça contato...')
+    changeTitle('Faça contato')
 
-    $(document).on('submit', '#cForm', sendContact)
+    /**
+     * Observer do formulário de contatos.
+     * Quando o formulário for enviado (onsubmit), executa a função
+     * sendContact().
+     */
+    $('#cForm').submit(sendContact)
 
 }
 
+/**
+ * Processa o envio do formulário de contatos.
+ */
 function sendContact(ev) {
 
+    // Bloqueia a ação normal do HTML, de enviar o formulário.
+    ev.preventDefault()
+
+    // Cria o objeto que vai armazenar dados de saída para a API.
     var formJSON = {}
-    console.log('++++', ev.target)
+
+    // Armazena o formulário como um objeto.
     const formData = new FormData(ev.target);
-    console.log('>>>', formData)
-    for (const [key, value] of formData) {
-        formJSON[key] = value
-    }
 
-    formJSON = JSON.stringify(formJSON)
+    // Obtém cada campo do formulário.
+    formData.forEach((value, key) => {
 
-    console.log(formJSON)
+        // Sanitiza o valor do campo e armazena no atributo do JSON.
+        formJSON[key] = value.trim()
 
-    // console.log(JSON.parse(formJSON))
+        // Atualiza o campo sanitizado, no formulário.
+        $('#' + key).val(formJSON[key])
+    })
 
+    // Envia os dados do formulário para a API.
+    $.post(app.apiContactsURL, formJSON)
+        .done((data) => {
+            var feedback;
+            if (data.status == 'success') {
+                var firstName = formJSON.name.split(' ')[0]
+                feedback = `
+                    <h3>Olá ${firstName}!</h3>
+                    <p>Seu contato foi enviado com sucesso.</p>
+                    <p>Obrigado...</p>
+                `
+            } else {
+                feedback = `
+                    <h3>Oooops!</h3>
+                    <p>Não foi possível enviar seu contato. Ocorreu uma falha no servidor.</p>
+                    <p><em><code>${data.data.message}</code></em></p>
+                `
+            }
+            $('#cForm').html(feedback)
+        })
+
+    delete formJSON
     return false
 }
