6ad90f91b7c66ad9e8f6e9a9041ee6d204d3cad4
Rafael Santos <rafaelsantux0411@gmail.com>
2023-05-11 19:50:29 -0300

    Update Api

--- index.js
+++ index.js
@@ -24,8 +24,8 @@
  const app = {
     siteName: 'FrontEndeiros',
     siteSlogan: 'Programando para o futuro',
-    // apiBaseURL: 'http://localhost:3000/'
-    apiBaseURL: 'https://frontendeirosapi.onrender.com/'
+    apiBaseURL: 'http://localhost/'
+    //apiBaseURL: 'https://frontendeirosapi.onrender.com/'
 }
 
 /**
